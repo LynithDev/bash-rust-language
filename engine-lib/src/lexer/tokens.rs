201f68eb68ca9206760b1c4f29f5af77e64fdcc2
LynithDev <61880709+LynithDev@users.noreply.github.com>
2025-01-26 22:29:25 +0100

    comment and plan out tokens

--- engine-lib/src/lexer/tokens.rs
+++ engine-lib/src/lexer/tokens.rs
@@ -2,11 +2,26 @@
 #[derive(Debug, PartialEq, Clone)]
 pub enum TokenType {
     // keywords
-    Var, // 'var'
-    Function, // 'fn'
+    Var, // 'var'                   - e.g. var test = 5
+    Function, // 'fn'               - e.g. fn test(arg: String): Int {...}
+    For, // 'for'                   - e.g. for i = 0, i < 5, i += 1 { ... }     OR  for i in 0..5 {...}     OR  for {...}
+    If, // 'if'                     - e.g. if condition {...}
+    Else, // 'else'                 - e.g. if condition {...} else if {...} else {...}
+    Match, // 'match'               - e.g. match var { pattern => {...}, pattern || pattern => {...} }
+    Break, // 'break'       
+    Continue, // 'continue'
+    Return, // 'return'
+    In, // 'in'
+    
+    
+    // special
+    Include, // '@include'          - e.g. '@include "path"'
+    Const, // '@const'              - e.g. '@const var NAME = value' OR '@const fn test() {...}'
+    
+
 
     // operator tokens
-    Equal, // '='
+    Equal, // 'is' OR '='
     Plus, // '+'
     Minus, // '-'
     Divide, // '/'
@@ -16,15 +31,25 @@ pub enum TokenType {
     MinusEqual, // '-='
     DivideEqual, // '/=,
     MultiplyEqual, // '*=',
-    NotEqual, // '!=',
-    Not, // '!'
+    NotEqual, // 'is not' OR '!=',
+    Not, // 'not' OR '!'
+    And, // 'and' OR '&&'
+    Or, // 'or' OR '||'
 
-    // literals
-    Identifier(String),
-    String(String),
-    Integer(isize),
+    // punctuation
+    LParam, // '('
+    RParam, // ')'
+    LBracket, // '{'
+    RBracket, // '}'
+    Comma, // ','
+    Colon, // ':'
+
+    // literals & types
+    Identifier(String), 
+    String(String), // '"..."'
+    Integer(isize), // e.g. '1' OR '-5'
     Boolean(bool), // 'true' OR 'false'
-    ShellCommand(String, Option<String>), // $"........"
+    ShellCommand(String, Option<String>), // '$cmd arg1 arg2' OR '$cmd(arg1 arg2)'
 
     EOL,
 }
